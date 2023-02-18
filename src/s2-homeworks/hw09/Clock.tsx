eec803e5ae98b2fbd81d18b7b4cf3826ed6efc58
kibeshev87 <mrjimb5@gmail.com>
2023-02-18 08:27:07 +0300

    hw8 updateStyle

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -29,17 +29,15 @@ function Clock() {
     }
 
     const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
-        console.log('onMouseEnter')
         setShow(true)
     }
     const onMouseLeave = () => { // пишут студенты // спрятать дату если мышка не наведена
-        console.log('onMouseLeave')
         setShow(false)
     }
 
     const stringTime = date.toLocaleTimeString() // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date.toLocaleDateString() // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
+    const stringDate = date.toLocaleDateString() // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
     const stringDay = new Intl.DateTimeFormat('en', {weekday: 'long'}).format(date) // пишут студенты
     const stringMonth = new Intl.DateTimeFormat('en', {month: 'long'}).format(date) // пишут студенты
@@ -62,8 +60,8 @@ function Clock() {
                 <div className={s.more}>
                     {show ? (
                         <>
-                            <span id={'hw9-month'}>{stringMonth}</span>,{' '}
-                            <span id={'hw9-date'}>{stringDate}</span>
+                            <span id={'hw9-month'}>{stringDate}</span>,{' '}
+                            <span id={'hw9-date'}>{stringMonth}</span>
                         </>
                     ) : (
                         <>
